e7eb44ee35a47c4b9b716db43971737055f60502
ddimucci <36259203+ddimucci@users.noreply.github.com>
2018-03-26 15:41:01 -0400

    Update barplots.R

--- MachineLearning_PairwiseCommunities/Barplots/barplots.R
+++ MachineLearning_PairwiseCommunities/Barplots/barplots.R
@@ -1,6 +1,6 @@
 # 17 Jan 2018
 # Demetrius DiMucci
-# Create Barplots For Fig 5
+# Create Barplots For Fig S2
 
 library(forestFloor)
 library(randomForest)
@@ -23,13 +23,6 @@ ff = forestFloor(mod1,Y,binary_reg = T,calc_np=T,bootstrapFC = F)
 X <- ff$FCmatrix
 colnames(X) <- aminos
 
-# Find which rows of X correspond to Proline with Serine
-target <- which(Ixns[,3] == 'P' & Ixns[,4] == 'S')
-pdf(file='Proline_Serine.pdf')
-par(mar=c(5,5,5,5))
-barplot(X[target,],names=aminos,ylim=range(X[target,]),las=3,ylab='Feature Contribution',
-        cex.names= 1.25,cex.lab=2.5,cex.axis = 2)
-dev.off()
 # Find which rows of X correspond to Methionine with Cysteine
 target <- which(Ixns[,3] == 'M' & Ixns[,4] == 'C')
 pdf(file='Methionine_Cysteine.pdf')
